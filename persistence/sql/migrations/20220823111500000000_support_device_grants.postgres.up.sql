98a7272242dfd4404017afad629b44f35793c396
Romain Caire <romain.caire@shadow.tech>
2022-09-30 16:30:57 +0200

    Don't support anything else than Postgres

--- persistence/sql/migrations/20220823111500000000_support_device_grants.postgres.up.sql
+++ persistence/sql/migrations/20220823111500000000_support_device_grants.postgres.up.sql
@@ -12,11 +12,12 @@ CREATE TABLE IF NOT EXISTS hydra_oauth2_device_code
     active             BOOL         NOT NULL DEFAULT true,
     requested_audience TEXT         NULL DEFAULT '',
     granted_audience   TEXT         NULL DEFAULT '',
-    challenge_id       VARCHAR(40)  NULL REFERENCES hydra_oauth2_consent_request_handled (challenge) ON DELETE CASCADE
+    challenge_id       VARCHAR(40)  NULL
 );
 CREATE INDEX hydra_oauth2_device_code_request_id_idx ON hydra_oauth2_device_code (request_id);
 CREATE INDEX hydra_oauth2_device_code_client_id_idx ON hydra_oauth2_device_code (client_id);
 CREATE INDEX hydra_oauth2_device_code_challenge_id_idx ON hydra_oauth2_device_code (challenge_id);
+ALTER TABLE hydra_oauth2_device_code ADD CONSTRAINT hydra_oauth2_device_code_challenge_id_fk FOREIGN KEY (challenge_id) REFERENCES hydra_oauth2_consent_request_handled(challenge) ON DELETE CASCADE;
 ALTER TABLE hydra_oauth2_device_code ADD CONSTRAINT hydra_oauth2_device_code_client_id_fk FOREIGN KEY (client_id) REFERENCES hydra_client(id) ON DELETE CASCADE;
 
 CREATE TABLE IF NOT EXISTS hydra_oauth2_user_code 
@@ -33,9 +34,10 @@ CREATE TABLE IF NOT EXISTS hydra_oauth2_user_code
     active             BOOL         NOT NULL DEFAULT true,
     requested_audience TEXT         NULL DEFAULT '',
     granted_audience   TEXT         NULL DEFAULT '',
-    challenge_id       VARCHAR(40)  NULL REFERENCES hydra_oauth2_consent_request_handled (challenge) ON DELETE CASCADE
+    challenge_id       VARCHAR(40)  NULL
 );
 CREATE INDEX hydra_oauth2_user_code_request_id_idx ON hydra_oauth2_user_code (request_id);
 CREATE INDEX hydra_oauth2_user_code_client_id_idx ON hydra_oauth2_user_code (client_id);
 CREATE INDEX hydra_oauth2_user_code_challenge_id_idx ON hydra_oauth2_user_code (challenge_id);
+ALTER TABLE hydra_oauth2_user_code ADD CONSTRAINT hydra_oauth2_user_code_challenge_id_fk FOREIGN KEY (challenge_id) REFERENCES hydra_oauth2_consent_request_handled(challenge) ON DELETE CASCADE;
 ALTER TABLE hydra_oauth2_user_code ADD CONSTRAINT hydra_oauth2_user_code_client_id_fk FOREIGN KEY (client_id) REFERENCES hydra_client(id) ON DELETE CASCADE;
--- persistence/sql/migrations/20220824111500000000_support_device_grant_consent.postgres.up.sql
+++ persistence/sql/migrations/20220824111500000000_support_device_grant_consent.postgres.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE hydra_oauth2_device_grant_request
     challenge             VARCHAR(40)  NOT NULL PRIMARY KEY,
     requested_scope       TEXT         NOT NULL,
     verifier              VARCHAR(40)  NOT NULL UNIQUE,
-    client_id             VARCHAR(255) NULL REFERENCES hydra_client (id) ON DELETE CASCADE,
+    client_id             VARCHAR(255) NULL,
     request_url           TEXT         NOT NULL,
     requested_audience    VARCHAR(255) NULL     DEFAULT '',
     csrf                  VARCHAR(40)  NOT NULL,
@@ -14,3 +14,4 @@ CREATE TABLE hydra_oauth2_device_grant_request
 CREATE INDEX hydra_oauth2_device_grant_request_client_id_idx ON hydra_oauth2_device_grant_request (client_id);
 CREATE INDEX hydra_oauth2_device_grant_request_verifier_idx ON hydra_oauth2_device_grant_request (verifier);
 CREATE INDEX hydra_oauth2_device_grant_request_challenge_idx ON hydra_oauth2_device_grant_request (challenge);
+ALTER TABLE hydra_oauth2_device_grant_request ADD CONSTRAINT hydra_oauth2_device_grant_request_client_id_fk FOREIGN KEY (client_id) REFERENCES hydra_client(id) ON DELETE CASCADE;
